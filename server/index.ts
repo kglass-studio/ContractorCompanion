c5ff0517162c0ae4ad928ef62d0548e3f67ac29b
kglass-studio <glass.kathyk@gmail.com>
2025-05-27 15:15:44 -0600

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,5 @@
 import express, { type Request, Response, NextFunction } from "express";
-//import { registerRoutes } from "./routes";
+import { registerRoutes } from "./routes";
 //import { setupVite, serveStatic, log } from "./vite";
 //import { initializeNotificationSystem } from "./notifications";
 // Attempt to force inclusion of the Linux-specific Rollup package
