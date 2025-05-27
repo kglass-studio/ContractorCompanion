1d68740e6806fedb4b66b4ead226d2dccc715fde
kglass-studio <glass.kathyk@gmail.com>
2025-05-27 15:42:46 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,15 +7,8 @@ export default defineConfig({
   base: './',
   plugins: [
     react(),
-    runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-        ]
-      : []),
+   // runtimeErrorOverlay(),
+    
   ],
   resolve: {
     alias: {
