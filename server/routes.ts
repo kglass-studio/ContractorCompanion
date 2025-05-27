8846b9638942688a2107415396f03bd8416cdae3
kglass-studio <glass.kathyk@gmail.com>
2025-05-27 17:54:22 -0600

    storage.tx comment outs

--- server/routes.ts
+++ server/routes.ts
@@ -1,6 +1,6 @@
 import express, { type Express, Request, Response } from "express";
 import { createServer, type Server } from "http";
-//import { storage, MemStorage } from "./storage";
+import { storage, MemStorage } from "./storage";
 //import {
  // insertClientSchema,
  // insertNoteSchema,
