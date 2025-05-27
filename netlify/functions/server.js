b71cb23bd26e643e70f51e89bb031a6a426a028e
kglass-studio <glass.kathyk@gmail.com>
2025-05-27 12:20:53 -0600

    Update server.js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,4 +1,5 @@
 export const handler = async (event, context) => {
+    console.error("Intentional Error for Testing Logs");
     const { default: expressApp } = await import('../../dist/index.js');
     const { default: serverless } = await import('serverless-http');
     const handler = serverless(expressApp);
